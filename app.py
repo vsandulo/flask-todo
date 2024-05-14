6e7ab6eaf7d324a67353fb2ed8e270e8f323e6b1
Valerii Sandulov <vsandulo@duosecurity.com>
2024-05-14 13:54:07 +0300

    Add swagger

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, jsonify
+from flasgger import Swagger, swag_from
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SWAGGER'] = {
+    'title': 'Todo API',
+    'uiversion': 3,
+    'openapi': '3.0.2'
+}
+swagger = Swagger(app, template=app.config['SWAGGER'])
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
@@ -11,17 +18,29 @@ class Todo(db.Model):
     title = db.Column(db.String(100))
     complete = db.Column(db.Boolean) 
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
     todo_list = Todo.query.all()
     print(todo_list)
     return render_template('base.html', todo_list=todo_list)
 
-@app.route('/about')
-def about():
-    return "About page!"
-
 @app.route('/add', methods=["POST"])
+@swag_from({
+    'responses': {
+        302: {
+            'description': 'Redirects to the main page after adding a new todo item.',
+        }
+    },
+    'parameters': [
+        {
+            'name': 'title',
+            'in': 'formData',
+            'type': 'string',
+            'required': True,
+            'description': 'Title of the new todo item'
+        }
+    ]
+})
 def add():
     title = request.form.get('title')
     new_todo = Todo(title=title, complete=False)
@@ -31,6 +50,22 @@ def add():
 
 
 @app.route('/update/<int:todo_id>')
+@swag_from({
+    'responses': {
+        302: {
+            'description': 'Redirects to the main page after toggling the completion status of a todo item.',
+        }
+    },
+    'parameters': [
+        {
+            'name': 'todo_id',
+            'in': 'path',
+            'type': 'integer',
+            'required': True,
+            'description': 'ID of the todo item to update'
+        }
+    ]
+})
 def update(todo_id):
     todo = Todo.query.filter_by(id=todo_id).first()
     todo.complete = not todo.complete
@@ -39,6 +74,22 @@ def update(todo_id):
 
 
 @app.route('/delete/<int:todo_id>')
+@swag_from({
+    'responses': {
+        302: {
+            'description': 'Redirects to the main page after deleting a todo item.',
+        }
+    },
+    'parameters': [
+        {
+            'name': 'todo_id',
+            'in': 'path',
+            'type': 'integer',
+            'required': True,
+            'description': 'ID of the todo item to delete'
+        }
+    ]
+})
 def delete(todo_id):
     todo = Todo.query.filter_by(id=todo_id).first()
     db.session.delete(todo)
